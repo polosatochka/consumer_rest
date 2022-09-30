9e68a86d1850be0b8e9da725046acc3590ddedf9
anna <s.anuna1401@gmail.com>
2022-09-30 21:16:55 +0500

    logging was added using @Log4j2 annotation

--- src/main/java/ru/anna/rest_dealer/controller/ConsumerController.java
+++ src/main/java/ru/anna/rest_dealer/controller/ConsumerController.java
@@ -1,5 +1,6 @@
 package ru.anna.rest_dealer.controller;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,10 +10,24 @@ import ru.anna.rest_dealer.model.Request;
 import ru.anna.rest_dealer.model.Response;
 
 @RestController
+@Log4j2
 public class ConsumerController {
+
+  /*  LOG LEVELS
+
+        public String logging() {
+        log.trace("A TRACE Message");
+        log.debug("A DEBUG Message");
+        log.info("An INFO Message");
+        log.warn("A WARN Message");
+        log.error("An ERROR Message");
+        return "Howdy! Check out the Logs to see the output...";
+    } */
 
     @PostMapping(value = "/feed")
     public ResponseEntity<Response> feedback(@RequestBody Request request){
+
+        log.info("Получаем ответ от первого сервиса...");
 
         Response response = Response.builder()
                 .uId(request.getUId())
@@ -23,9 +38,12 @@ public class ConsumerController {
                 .errorMessage("")
                 .build();
 
-        System.out.println(response);
-        System.out.println("Ответ доставлен успешно");
+     //   System.out.println(response);
+        log.info(response);
+      //  System.out.println("Ответ доставлен успешно");
+        log.info("Ответ получен успешно");
          return new ResponseEntity<>(response, HttpStatus.OK);
-
     }
+
+
 }
